12ec83b6c875d5081d5662fdc41c2126cdf874a8
WRS <shadowpriest87@gmail.com>
2016-06-10 22:24:46 +0500

    Start

---
+++ Scripts/Defender.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defender : MonoBehaviour {
+
+	public bool haveBlood;
+	public bool haveDust;
+	public int price = 100;
+
+	private StarDisplay starDisplay;
+
+	// Use this for initialization
+	void Start () {
+		starDisplay = GameObject.FindObjectOfType<StarDisplay>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddStars(int count){
+		starDisplay.AddStars (count);
+
+	}
+
+//  Пока не используется
+//	void OnTriggerEnter2D(){
+//		Debug.Log(gameObject + "triggered enter ");
+//	}
+
+}
